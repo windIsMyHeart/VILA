ad5d014292d1fa5686c3d19cea820b757fac4753
Huafeng Shi <75714578@qq.com>
2024-08-01 16:05:04 +0800

    fix py 3.9

--- llava/train/utils.py
+++ llava/train/utils.py
@@ -35,7 +35,7 @@ def is_local(model_name_or_path: str) -> bool:
 
 def get_checkpoint_path(
     output_dir: str, checkpoint_prefix: str = "checkpoint"
-) -> str | None:
+) -> str:
     output_dir = os.path.abspath(output_dir)
     pathlib_dir = pathlib.Path(output_dir)
 
